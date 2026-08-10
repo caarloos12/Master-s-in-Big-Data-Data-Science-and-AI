dbf8d3171a8d9d84146693227537d30404bc09e2
caarloos12 <c.sanroman.cvg@gmail.com>
2026-08-10 14:16:16 +0200

    Add files via upload

--- 02_Machine_Learning/02_Data_Mining/CarlosSanRomanCazorla_DataMining.py
+++ 02_Machine_Learning/02_Data_Mining/CarlosSanRomanCazorla_DataMining.py
@@ -1,1 +1,21 @@
-# File moved from 02_Machine_Learning/Data_Mining/CarlosSanRomanCazorla_DataMining.py
+# Cargo las librerias 
+import os
+import pickle
+from sklearn.model_selection import train_test_split
+import itertools
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+# Cargo las funciones que voy a utilizar
+from FuncionesMineria import (analizar_variables_categoricas, cuentaDistintos, frec_variables_num, 
+                           atipicosAmissing, patron_perdidos, ImputacionCuant, ImputacionCuali)
+
+# Cargo los datos
+datos = pd.read_excel('Tarea\DatosEleccionesEspaña.xlsx')
+
+# Comprobamos el tipo de formato de las variables variable que se ha asignado en la lectura.
+# No todas las categoricas estan como queremos
+datos.dtypes
+
